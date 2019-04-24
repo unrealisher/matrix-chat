5543c3ca26b52bbb1c7105d5026ea8998b6aa810
unrealisher <unrealmpa3@mail.ru>
2019-04-24 23:41:52 +0300

    Убрал лишнее

--- server/index.js
+++ server/index.js
@@ -1,8 +1,4 @@
 const WebSocket = require("ws");
-const express = require("express");
-const path = require("path");
-
-const port = process.env.PORT || 8080;
 
 const app = new WebSocket.Server({ port: 8000 });
 
@@ -15,12 +11,3 @@ app.on("connection", ws => {
     });
   });
 });
-
-const server = express();
-server.use(express.static(path.resolve("../build")));
-
-server.get("*", (req, res) => {
-  res.sendFile(path.resolve("../build/index.html"));
-});
-
-server.listen(port);
